01105f7cd40c94f71dffc79c81b039368297b1b5
SHENGNANHAI <46678109+SHENGNANHAI@users.noreply.github.com>
2020-02-21 06:36:50 +0000

    Update models.py

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -16,9 +16,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
     
     def __str__(self):
-        return self.name
-		
-		
+        return self.name		
 		
 
 class Page(models.Model):
